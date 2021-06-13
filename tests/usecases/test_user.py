90bed9846ea6eabad763d13754ae6c83a5d16043
Jaehyeon Kim <dottami@gmail.com>
2021-06-14 00:04:25 +1000

    add get_user test cases

--- tests/usecases/test_user.py
+++ tests/usecases/test_user.py
@@ -1,3 +1,4 @@
+import pytest
 import uuid
 from datetime import datetime
 
@@ -26,4 +27,23 @@ def test_create_user(items_table):
     from messageboard.usecases.user import create_user
 
     user = User.from_dict(user_dict)
-    assert create_user(user) == user_dict
+    assert create_user(user) == user
+
+
+def test_get_user_success(items_table):
+    from messageboard.usecases.user import create_user, get_user
+
+    user = User.from_dict(user_dict)
+    create_user(user)
+
+    assert get_user("john.doe@email.com") == user
+
+
+def test_get_user_failure(items_table):
+    from messageboard.usecases.user import create_user, get_user
+
+    user = User.from_dict(user_dict)
+    create_user(user)
+
+    with pytest.raises(IndexError):
+        get_user("jane.doe@email.com")
