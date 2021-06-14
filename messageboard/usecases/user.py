6b72834f59a59d2b4f0aec2b3abf144da27209bb
Jaehyeon Kim <dottami@gmail.com>
2021-06-14 16:02:30 +1000

    add tracer.capture_method

--- messageboard/usecases/user.py
+++ messageboard/usecases/user.py
@@ -1,16 +1,21 @@
 import os
 from boto3.dynamodb.conditions import Key
+from aws_lambda_powertools import Tracer
 
 from messageboard.models.models import User
 from messageboard.utils.common import get_table
 
+tracer = Tracer()
 
+
+@tracer.capture_method
 def create_user(user: User):
     items_table = get_table()
     items_table.put_item(Item=user.to_dict())
     return user
 
 
+@tracer.capture_method
 def get_user(email: str):
     items_table = get_table()
     resp = items_table.query(
